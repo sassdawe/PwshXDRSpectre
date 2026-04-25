ada308220f48f542cc5cefe1723bcb2bc8a5a19a
David Sass <elethulyelenseg@hotmail.com>
2026-04-25 18:39:03 +0200

    misc(cleanup) old entry points

--- src/Tests/PwshXDRDashboard.Tests.ps1
+++
@@ -1,9 +0,0 @@
-BeforeAll {
-    $script:legacyScript = Get-Content -Path "$PSScriptRoot/../PwshXDRDashboard.ps1" -Raw
-}
-
-Describe 'PwshXDRDashboard wrapper' {
-    It 'calls Start-PwshXdrLiveDashboard with expected parameters' {
-        $script:legacyScript | Should -Match 'Start-PwshXdrLiveDashboard\s+-TenantId\s+\$tenant\s+-ClientId\s+\$clientID\s+-Limit\s+\$limit\s+-UseDeviceCode:\$UseDeviceCode\.IsPresent'
-    }
-}
